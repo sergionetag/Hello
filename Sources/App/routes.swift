8a2369870aa13941733a048f833c613991a9fe7c
Sergio Furlaneto Filho <peo_sfurlaneto@uolinc.com>
2019-01-02 13:11:53 -0200

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,6 +11,10 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    router.get("user") {request in
+        return "Users API"
+    }
 
     // Example of configuring a controller
     let todoController = TodoController()
